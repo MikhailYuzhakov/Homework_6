ac536c57ea206a6e1b9722875c8bb7743e3a5baf
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-05-06 21:47:46 +0300

    Кол-во введенных положительных чисел

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+
+int i = 0;
+int[] array = new int[1000];
+
+Console.Clear();
+Console.WriteLine("Введите M чисел с клавиатуры через пробел: ");
+
+int[] mas = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (int j = 0; j < mas.Length; j++) if (mas[j] > 0) i++;
+
+Console.WriteLine($"Количество положительных чисел = {i}");
